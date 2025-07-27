43ae7f7367ec92769e3c02368082d184c2e43655
Mone't Fulgham <monetfulgham@gmail.com>
2025-07-27 14:45:32 -0400

    Add OpenAI Cloudflare worker for Pollyglot project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from "openai";
 
-import openai from "openai";
-
-const ai = new openai({apiKey: process.env.OPENAI_API_KEY})
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello from my Open AI API Worker!');
+		const corsHeaders = { "Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "POST, OPTIONS", "Access-Control-Allow-Headers": "content-type"
+	};
+		//Handle CORS Preflight
+		if(request.method === "OPTIONS") {
+			return new Response(null, {headers: corsHeaders})
+		}
+
+		const openAi = new OpenAI({apiKey: env.OPENAI_API_KEY})
+		const messages = await request.json();
+		try {
+			const chatCompletion = await openAi.chat.completions.create({
+				model: "gpt-4.1-nano-2025-04-14",
+				temperature: 1.1,
+				messages,
+			})
+			return new Response(JSON.stringify(chatCompletion.choices[0].message), {headers: corsHeaders} );
+		}	catch (e) {
+			console.error("OpenAI Error ", e)
+			return new Response(e, {headers: corsHeaders} )
+		}
 	},
 };
