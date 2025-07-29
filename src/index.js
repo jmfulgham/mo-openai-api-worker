01014645a399146f3f2a8c5266dd566d8d2ba81a
Mone't Fulgham <monetfulgham@gmail.com>
2025-07-29 16:14:58 -0400

    Add AI Gateway

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import OpenAI from "openai";
+import OpenAI from 'openai';
 
 export default {
 	async fetch(request, env, ctx) {
-		const corsHeaders = { "Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Methods": "POST, OPTIONS", "Access-Control-Allow-Headers": "content-type"
-	};
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'POST, OPTIONS', 'Access-Control-Allow-Headers': 'content-type'
+		};
 		//Handle CORS Preflight
-		if(request.method === "OPTIONS") {
-			return new Response(null, {headers: corsHeaders})
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
 		}
 
-		const openAi = new OpenAI({apiKey: env.OPENAI_API_KEY})
+		const openAi = new OpenAI({
+			apiKey: env.OPENAI_API_KEY,
+			baseURL: 'https://gateway.ai.cloudflare.com/v1/20ff3604bcf584d2dad44019542817a7/pollyglot/openai'
+		});
 		const messages = await request.json();
 		try {
 			const chatCompletion = await openAi.chat.completions.create({
-				model: "gpt-4.1-nano-2025-04-14",
+				model: 'gpt-4.1-nano-2025-04-14',
 				temperature: 1.1,
-				messages,
-			})
-			return new Response(JSON.stringify(chatCompletion.choices[0].message), {headers: corsHeaders} );
-		}	catch (e) {
-			console.error("OpenAI Error ", e)
-			return new Response(e, {headers: corsHeaders} )
+				messages
+			});
+			return new Response(JSON.stringify(chatCompletion.choices[0].message), { headers: corsHeaders });
+		} catch (e) {
+			console.error('OpenAI Error ', e);
+			return new Response(e, { headers: corsHeaders });
 		}
-	},
+	}
 };
